91b2234dd7f9bece6572d574184bf4ab77bc72f5
David de Lucas <delucas.david@gmail.com>
2020-05-07 19:01:14 +0200

    complete TestSquareVerifier.js

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.21 <0.6.0;
 
 import 'openzeppelin-solidity/contracts/utils/Address.sol';
 import 'openzeppelin-solidity/contracts/drafts/Counters.sol';
--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -30,16 +30,26 @@ contract('TestERC721Mintable', accounts => {
         })
 
         it('should get token balance', async function () { 
-            
+            const result = await this.contract.balanceOf.call(account_one);
+            assert.equal(result, 8, "Incorrect token balance for account #1" );
+
+            const result2 = await this.contract.balanceOf.call(account_two);
+            assert.equal(result2,0,"Incorrect token balance for account #2");
         })
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () { 
-            
+            const result = await this.contract.tokenURI(1);
+            assert.equal(result, 'https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1' , "Incorrect URI");
+          
         })
 
         it('should transfer token from one owner to another', async function () { 
-            
+            await this.contract.approve(account_two, 1);
+            await this.contract.transferFrom(account_one, account_two, 1);
+
+            const _owner = await this.contract.ownerOf(1);
+            assert.equal(_owner,account_two, "Incorrect owner for token, should be account #2");
         })
     });
 
@@ -49,11 +59,19 @@ contract('TestERC721Mintable', accounts => {
         })
 
         it('should fail when minting when address is not contract owner', async function () { 
-            
+            var result = false;
+            try {
+                await this.contract.mint(account_one, 1, { from: account_two });
+            } catch (e) {
+                console.log(e.reason);
+                result = true;
+            }
+            assert.equal(result,true,"Mint should fail as the address is not contract owner");
         })
 
         it('should return contract owner', async function () { 
-            
+            const result = await this.contract.getOwner.call();
+            assert.equal(result, account_one,"Account # 1 should be the owner of the contract");            
         })
 
     });
