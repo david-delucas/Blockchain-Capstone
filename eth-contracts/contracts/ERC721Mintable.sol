0c995c2d8c02ebf811a5dadddb4c0f08fa2bc103
David de Lucas <delucas.david@gmail.com>
2020-05-07 12:17:37 +0200

    update README

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -38,11 +38,18 @@ contract Ownable {
 }
 
 //  TODO's: Create a Pausable contract that inherits from the Ownable contract
-//  1) create a private '_paused' variable of type bool
+contract Pausable is Ownable {
+
+    //  1) create a private '_paused' variable of type bool
+    bool private _paused;
+
 //  2) create a public setter using the inherited onlyOwner modifier 
 //  3) create an internal constructor that sets the _paused variable to false
 //  4) create 'whenNotPaused' & 'paused' modifier that throws in the appropriate situation
 //  5) create a Paused & Unpaused event that emits the address that triggered the event
+
+
+}
 
 contract ERC165 {
     bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
